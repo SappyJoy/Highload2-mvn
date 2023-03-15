cb0218fec3e30ce754be1f7a45dc978a2429b442
Stepan Ponomarev <stponomarevw@gmail.com>
2023-03-15 23:47:57 +0300

    fix: Annotate UserServiceApplication with SpringBootApplication

--- user-service/src/main/java/com/highload/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/highload/userservice/UserServiceApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
-//@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 @ComponentScan(basePackages = {"com.highload.userservice","com.highload.feign"})
 @EnableDiscoveryClient
+@SpringBootApplication
 public class UserServiceApplication {
 
     public static void main(String[] args) {
